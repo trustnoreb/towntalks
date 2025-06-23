2b009cc8e59d7ddc15f0a369fd375a5f2ca03002
trustnoreb <trustnoreb@users.noreply.github.com>
2025-06-23 19:15:40 +0200

    Added while hover animation on arrow

--- src/app/components/card/index.tsx
+++ src/app/components/card/index.tsx
@@ -1,5 +1,9 @@
-import css from "./card.module.css";
+"use client";
 
+import Image from "next/image";
+import css from "./card.module.css";
+import { motion } from "framer-motion";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 interface Props {
   data: {
     name: string;
@@ -10,11 +14,29 @@ interface Props {
 function Card({ data }: Props) {
   return (
     <div className={css.container}>
+      <Image
+        src={data.photo}
+        alt={data.name}
+        width={450}
+        height={300}
+        className={css.card_image}
+      />
       <div className={css.name}>
         <h2>{data.name}</h2>
       </div>
-      <div className={css.class}>{data.photo}</div>
-      <div className={css.class}>{data.icon}</div>
+      <div className={css.discover_container}>
+        <div className={css.discover}>
+          <motion.div
+            className={css.arrow}
+            initial={{ marginRight: "0" }}
+            whileHover={{ marginRight: "10px" }}
+            transition={{ ease: "easeOut", duration: 0.5 }}
+          >
+            Scopri
+          </motion.div>
+          <ArrowForwardIcon />
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/components/cards/index.tsx
+++ src/app/components/cards/index.tsx
@@ -2,11 +2,24 @@ import Card from "../card";
 import css from "./cards.module.css";
 
 const data = [
-  { name: "Sara", icon: "", photo: "" },
-  { name: "Marco", icon: "", photo: "" },
-  { name: "Carla", icon: "", photo: "" },
-  { name: "Ettore", icon: "", photo: "" },
-  { name: "Antonella", icon: "", photo: "" },
+  { name: "Mario Rossi", icon: "", photo: "/images/candidati/mario_rossi.png" },
+  {
+    name: "Elena Moretti",
+    icon: "",
+    photo: "/images/candidati/eleonora_moretti.png",
+  },
+  {
+    name: "Luca Bianchi",
+    icon: "",
+    photo: "/images/candidati/luca_bianchi.png",
+  },
+  {
+    name: "Chiara rinaldi",
+    icon: "",
+    photo: "/images/candidati/chiara_rinaldi.png",
+  },
+  { name: "Paolo Blu", icon: "", photo: "/images/candidati/paolo_blu.png" },
+  { name: "Carla Verdi", icon: "", photo: "/images/candidati/carla_verdi.png" },
 ];
 function Cards() {
   return (
